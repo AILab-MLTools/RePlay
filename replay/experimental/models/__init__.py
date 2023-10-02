737bf518032ad0eb2ad111db7de95d77c2c3f209
eabodrov <eabodrov@sberbank.ru>
2023-10-02 10:57:46 +0300

    Move LightFM to experimental

--- replay/experimental/models/__init__.py
+++ replay/experimental/models/__init__.py
@@ -1,3 +1,4 @@
 from replay.experimental.models.admm_slim import ADMMSLIM
 from replay.experimental.models.als import ALSWrap
 from replay.experimental.models.implicit_wrap import ImplicitWrap
+from replay.experimental.models.lightfm_wrap import LightFMWrap
