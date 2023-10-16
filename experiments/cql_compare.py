535a19593aefd18b901bb3cdb2e2688b36cc7fd2
eabodrov <eabodrov@sberbank.ru>
2023-10-16 13:15:36 +0300

    Replace ScalaALSWrap to ALSWrap core in experiments

--- experiments/cql_compare.py
+++ experiments/cql_compare.py
@@ -18,8 +18,8 @@ from replay.metrics.experiment import Experiment
 from replay.experimental.models.cql import MdpDatasetBuilder
 from replay.metrics import HitRate, NDCG, MAP, MRR, Coverage, Surprisal
 from replay.utils.model_handler import save, load
-from replay.models import UCB, Wilson, Recommender, ItemKNN, SLIM
-from replay.experimental.models import LightFMWrap, ScalaALSWrap, CQL
+from replay.models import UCB, Wilson, Recommender, ItemKNN, SLIM, ALSWrap
+from replay.experimental.models import LightFMWrap, CQL
 from replay.utils.session_handler import State, get_spark_session
 from replay.splitters import DateSplitter
 from replay.utils import get_log_info
@@ -166,7 +166,7 @@ def main():
     }
 
     algorithms.update({
-        'ALS': ScalaALSWrap(seed=SEED),
+        'ALS': ALSWrap(seed=SEED),
         'KNN': ItemKNN(num_neighbours=K),
         'LightFM': LightFMWrap(random_state=SEED),
         'SLIM': SLIM(seed=SEED),
