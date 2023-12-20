ef8b6d4edb44e05a1ad8eb730068953260817d62
eabodrov <eabodrov@sberbank.ru>
2023-12-20 10:17:11 +0300

    Replace convertions

--- replay/models/extensions/ann/index_builders/driver_hnswlib_index_builder.py
+++ replay/models/extensions/ann/index_builders/driver_hnswlib_index_builder.py
@@ -9,6 +9,7 @@ from replay.models.extensions.ann.index_inferers.hnswlib_filter_index_inferer im
 from replay.models.extensions.ann.index_inferers.hnswlib_index_inferer import HnswlibIndexInferer
 from replay.models.extensions.ann.utils import create_hnswlib_index_instance
 from replay.utils import SparkDataFrame
+from replay.utils.spark_utils import spark_to_pandas
 
 logger = logging.getLogger("replay")
 
@@ -26,17 +27,19 @@ class DriverHnswlibIndexBuilder(IndexBuilder):
         else:
             return HnswlibIndexInferer(self.index_params, self.index_store)
 
+    # pylint: disable=no-member
     def build_index(
         self,
         vectors: SparkDataFrame,
         features_col: str,
         ids_col: Optional[str] = None,
     ):
-        vectors_np = np.array([np.array(x[features_col]) for x in vectors.select(features_col).collect()])
+        vectors = spark_to_pandas(vectors, self.allow_collect_to_master)
+        vectors_np = np.squeeze(vectors[features_col].values)
         index = create_hnswlib_index_instance(self.index_params, init=True)
 
         if ids_col:
-            index.add_items(np.stack(vectors_np), np.array([x[ids_col] for x in vectors.select(ids_col).collect()]))
+            index.add_items(np.stack(vectors_np), vectors[ids_col].values)
         else:
             index.add_items(np.stack(vectors_np))
 
